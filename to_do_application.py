00dc524fb63d594ec517cd8a4faa846a7defc325
Alejandra Cortes <alejandra.monserrat.ch@gmail.com>
2024-06-11 11:23:58 -0700

    Add remove function (refined)

--- to_do_application.py
+++ to_do_application.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 class ToDoApplication:
     def __init__(self):
-        self.tasks = list()
+        self.tasks = dict()
 
     def __del__(self):
         pass
@@ -11,10 +13,19 @@ class ToDoApplication:
             if usr_inpt == "":
                 print("You have to type something.")
                 continue
+            priority = input("Enter the priority (high, medium, low): ")
+            if priority == "":
+                print("You have to type something.")
+            elif priority not in ("high", "medium", "low"):
+                print("Type one of the three priority options")
+                continue
+            deadline = input(f"Enter the deadline (YYYY-MM-DD): ")
+            if deadline == "":
+                print("You have to type something.")
+                continue
             break
-
-        self.tasks.append(usr_inpt)
-        print(f"'{usr_inpt}' has been added to the list.")
+        print(f"'{usr_inpt}' with {priority} priority and deadline {deadline} has been added to the list.")
+        self.tasks[usr_inpt] = [priority, deadline]
 
     def remove_task(self):
         if not self.tasks:
@@ -23,8 +34,8 @@ class ToDoApplication:
 
         usr_inpt = input("Enter the task to remove: ")
         if usr_inpt in self.tasks:
-            self.tasks.remove(usr_inpt)
-            print(f"'{usr_inpt}' has been removed from the list.")
+            self.tasks.remove(usr_inpt) #User had to type the task name with priotity and date
+            print(f"'{usr_inpt}' has been removed from the list. Advanced")
         elif usr_inpt == "":
             print("You have to type something.")
         else:
@@ -37,13 +48,13 @@ class ToDoApplication:
         else:
             print("To-Do list")
 
-            for i in range(len(self.tasks)):
-                print(f"{i+1}.", self.tasks[i])
+            for k, v in self.tasks:
+                print(f"{k+1}.", self.tasks[k])
 
 
     def start(self):
         while True:
-            print("\nTo do Application\n"
+            print("\n Advanced To do Application\n"
                   "1. Add Task\n"
                   "2. Remove Task\n"
                   "3. View Tasks\n"
@@ -68,4 +79,7 @@ class ToDoApplication:
                 case 3:
                     self.view_task()
                 case 4:
+                    print("Goodbye!")
                     exit()
+
+ToDoApplication().start()
